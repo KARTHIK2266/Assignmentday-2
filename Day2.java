2a91b7a73228de48b2ae76c2f438d48d6179299a
KARTHIK2266 <93930752+KARTHIK2266@users.noreply.github.com>
2021-11-09 22:47:47 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,19 @@
+package Assignments;
+
+public class Day2 {
+
+	private int x;
+	private int y;
+
+	public static void main(String[] args) {
+		Day2 ob1 = new Day2();
+		Day2 ob2 = new Day2();
+		
+		ob1.x = 10;
+		ob2.y = 15;
+		
+		System.out.println("Ob1 Data:"+ob1);
+		System.out.println("Ob2 Data:"+ob2);
+	}
+
+}
